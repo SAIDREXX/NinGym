977d72d63851ef6ff6e3c98497cb09e0ede514a2
SAIDREXX <saidrexxdemher@gmail.com>
2023-09-13 23:11:13 -0600

    Main Page Base

--- index.php
+++ index.php
@@ -3,5 +3,36 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale-1.0">
-    <title>Inicio</title>
+    <title>NinGym</title>
+    <link rel="stylesheet" href="css/index_style.css">
 </head>
+<body>
+    <header>
+        <div class="navbar-container">
+            <nav>
+                <ul class="navbar-list">
+                    
+                    <li><a href="index.php">Inicio</a></li>
+                    <li><a href="acerca.html">Acerca de</a></li>
+                    <li><a href="servicios.html">Servicios</a></li>
+                    <li><a href="contacto.html">Contacto</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <main>
+        <section>
+            <h1>NinGym</h1>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+        </section>
+    </main>
+    <footer>
+        <p>&copy; 2023 SAIDREXXX</p>
+    </footer>
+</body>
+</html>
